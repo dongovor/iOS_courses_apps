e91ee0053f85647733f4bd5d81cdcfe84416a2cb
Dmitry Cherkasov <govor86@gmail.com>
2017-07-26 06:10:13 +0300

    no message

---
+++ SillySong/SillySong/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SillySong
+//
+//  Created by Dmitry Cherkasov on 7/22/17.
+//  Copyright © 2017 Dmitry Cherkasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func editingDidBegin(_ sender: Any) {
+    }
+
+    @IBAction func songGeneration(_ sender: Any) {
+    }
+    
+}
+
